552e44b7e63bc778105ce35b992600dbb4710b0f
Nicolai A. Dalaaker <dalaaker.nicolai@gmail.com>
2018-07-09 22:23:02 +0200

    added gameScreen class

---
+++ src/com/project5/screens/GameScreen.java
@@ -0,0 +1,64 @@
+package com.project5.screens;
+
+import com.project5.GameWindow;
+import com.project5.Screen;
+import com.project5.assets.AssetManager;
+import com.project5.gui.KeyboardEvent;
+import com.project5.gui.MouseButtonEvent;
+import com.project5.gui.MouseMotionEvent;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GameScreen implements Screen {
+
+    private GameWindow window;
+
+    public GameScreen(GameWindow window){
+        this.window = window;
+    }
+
+    @Override
+    public void update(Graphics g, long delta) {
+
+
+    }
+
+    @Override
+    public void render(Graphics g, long delta) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTransitionIn() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTransitionOut() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onMouseButton(MouseButtonEvent event) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onMouseMotion(MouseMotionEvent event) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onKeyboard(KeyboardEvent event) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+}
